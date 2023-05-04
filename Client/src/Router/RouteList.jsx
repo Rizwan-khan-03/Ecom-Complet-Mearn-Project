083fe94413bd754df3e063e42fc9a13ea347684d
Rizwan <eminence.rizwan@gmail.com>
2023-05-04 12:58:22 +0530

    mobile screen

--- Client/src/Router/RouteList.jsx
+++ Client/src/Router/RouteList.jsx
@@ -1,8 +1,19 @@
 
+import Appliances from "../Component/maincomponent/Apliances/Appliances";
+import Beauty from "../Component/maincomponent/Beauty/Beauty";
 import Groceriess from "../Component/maincomponent/Grocery/Groceriess";
 import Mobiles from "../Component/maincomponent/Mobiless/Mobiles";
 import Product from "../Component/maincomponent/Products/Product";
 import Login from "../Pages/Login";
+import applinces from './img/applinces.jpg'
+import electronics from './img/elec.jpg'
+import fashion from './img/fashion.jpg'
+import furniture from './img/furniture.jpg'
+import groce from './img/Groceriess.jpg'
+import moblies from './img/moblies.jpg'
+import beauty from './img/beauty.jpg'
+import home from './img/home.jpg'
+
 
 //UserDetails
 export const routepath = [
@@ -26,32 +37,52 @@ export const routepath = [
     Element: Mobiles,
     private: true,
   },
+  {
+    path: "/applances",
+    Element: Appliances,
+    private: true,
+  },
+  {
+    path: "/beauty",
+    Element: Beauty,
+    private: true,
+  },
 ];
 export const routeLink = [
   {
     link:"grocery",
     name:'Grocery',
-    className: 'anvDash_icon'
+    img: groce,
   },
   {
     link:'mobiles',
     name:'Mobiles',
-    className: 'anvComp_icon'
+    img: moblies
   },
   {
     link:'fashion',
     name:'fashion ',
-    className: 'anvMc_icon'
+    img: fashion
   },
   {
     link:'electronics',
     name:'Electronics',
-    className: 'anvUser_icon'
+    img: electronics
   },
   {
     link:'home',
     name:'Home',
-    className: 'anvVt_icon'
+    img: home
+  },
+  {
+    link:'applances',
+    name:'Applances',
+    img: furniture
+  },
+  {
+    link:'beauty',
+    name:'Beauty',
+    img: beauty
   },
  
 ]
